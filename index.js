960c8333b6e5c59d30797d9ffb8c6e39689111ac
unknown <guiratwalid98@gmail.com>
2022-07-03 14:35:49 +0100

    deployment

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ const path = require('path');
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, '../chat-app/build')));
+    app.use(express.static(path.join(__dirname1, 'chat-app/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, '../chat-app/build/index.html'));
+        res.sendFile(path.resolve(__dirname1, 'chat-app/build/index.html'));
     })
 }
 else {
